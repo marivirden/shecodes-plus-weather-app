e5d765a56e6cbfe52b63ba5678f372d91b6487e9
marivirden <97749369+marivirden@users.noreply.github.com>
2022-04-26 11:31:26 -0500

    Initial commit

---
+++ src/wireframe.js
@@ -0,0 +1,67 @@
+function getCurrentTemperature(cityName) {
+  const apiKey = "0c5cd3271ca5da27f88448575bac1056";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+
+  function showTemperature(response) {
+    console.log(response);
+
+    const temperature = Math.round(response.data.main.temp);
+    const temperatureElement = document.querySelector("#current-temperature");
+    temperatureElement.innerHTML = `${temperature}°F`;
+  }
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCity() {
+  const city = document.querySelector("#city-search-box").value;
+
+  console.log("city:", city);
+
+  const currentCity = document.querySelector("#ciudad");
+  currentCity.innerHTML = city;
+
+  getCurrentTemperature(city);
+}
+
+const button = document.querySelector("#button-addon2");
+button.addEventListener("click", getCity);
+
+const now = new Date();
+console.log(now);
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.getDay());
+console.log(now.getFullYear());
+console.log(now.getMonth());
+
+let h3 = document.querySelector("#date");
+
+const date = now.getDate();
+const days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+const day = days[now.getDay()];
+const year = now.getFullYear();
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const month = months[now.getMonth()];
+const hours = now.getHours();
+const minutes = now.getMinutes();
+
+// I found my answer at https://stackoverflow.com/a/18889674
+const hoursWithLeadingZeroIfNecessary = ("0" + hours).slice(-2);
+const minutesWithLeadingZeroIfNecessary = ("0" + minutes).slice(-2);
+
+h3.innerHTML = `${day}, ${month} ${date} ${year} - ${hoursWithLeadingZeroIfNecessary}:${minutesWithLeadingZeroIfNecessary}`;
